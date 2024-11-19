a6dfef1e26396bebaf25b365013e3ef684e2637d
git-edifice <git_edifice@yahoo.co.jp>
2024-11-19 18:32:53 +0900

    メタデーター/マージン/方向

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:pdf/pdf.dart';// PdfPageFormat
 import 'package:pdf/widgets.dart' as pw;
 import 'package:printing/printing.dart';
 
 void main() {
-
-    final buttonLayoutPdf = ElevatedButton(
-      onPressed: () async{ 
-        final pdf = await makePdf(); // 1
-        await Printing.layoutPdf( // 2 
-          onLayout: (_) async{
-            return await pdf.save(); // 3
-          },
-        );
-      },
-      child: const Text("印刷"), 
-    );
-    
-    final buttonSharePdf = ElevatedButton(
-      onPressed: () async{ 
+  final body = Center( // ボディー
+    child: PdfPreview(// プレビューの表示
+      build: (format) async {
         final pdf = await makePdf();
-        await Printing.sharePdf( // 4
-          bytes: await pdf.save(),
-          filename: 'test.pdf'
-        ,);
-      },
-      child: const Text("他のアプリケーションと共有"), 
-    );
-
-  final body = SafeArea( // ボディー
-    child: Column(
-      children: [buttonLayoutPdf, buttonSharePdf, ],
-    ),
+        // .save()で「Uint8List」形式の作成
+        return await pdf.save();
+      },),
   );  
 
-  final sc = Scaffold( body: body,);
+  final sc = Scaffold(
+    body: body,);
+
   final app = MaterialApp(home: sc);
   runApp(app);
 }
 
 Future makePdf() async {
-  final pdf = pw.Document();
+  // final pdf = pw.Document();
+  final pdf = pw.Document(
+    author: '作成者',
+    creator: 'クリエイター',
+    title: 'タイトル',
+    subject:  'サブジェクト',
+  );
+
   final page = pw.Page(
+    pageTheme: pw.PageTheme(
+      orientation:pw.PageOrientation.landscape, // 1
+      pageFormat: PdfPageFormat.a4.copyWith( // 2
+        marginTop: 20,
+        marginBottom: 20,
+        marginLeft: 20,
+        marginRight: 20,
+      ),
+    ),
     build: (pw.Context context) {
       return pw.Center(
         child: pw.Text("PDF Test"),
