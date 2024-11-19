8fd08ac2c49770c5f71d9705d9e416e1048449e7
git-edifice <git_edifice@yahoo.co.jp>
2024-11-19 18:28:36 +0900

    日本語フォントに対応する - 【１．日本語フォントをダウンロード】

--- lib/main.dart
+++ lib/main.dart
@@ -4,29 +4,33 @@ import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
 import 'package:printing/printing.dart';
 
+import 'package:flutter/services.dart'; // rootBundleで使用
+
 void main() {
   final body = Center( // ボディー
     child: PdfPreview(// プレビューの表示
-      allowPrinting: true, // 印刷許可だけ残す（これも含め全てfalseでプレビューワーのみになります）
-      allowSharing: false,
-      canChangeOrientation: false,
-      canChangePageFormat: false,
-      canDebug: false,
       build: (format) async {
-        final pdf = await makePdf();// PDF作成関数
+        final pdf = await makePdf();
         // .save()で「Uint8List」形式の作成
         return await pdf.save();
-      },
-    ),
+      },),
   );  
 
-  final sc = Scaffold( body: body, );
+  final sc = Scaffold(
+    body: body, // ボディー        
+  );
+
   final app = MaterialApp(home: sc);
   runApp(app);
 }
-// PDF作成関数
-Future makePdf() async {
+
+Future makePdf() async { // PDF作成
+
+  final fontData = await rootBundle.load('assets/fonts/ShipporiMincho-Regular.ttf');
+  final font = pw.Font.ttf(fontData);
+
   final pdf = pw.Document();
+  
   final page = pw.Page(
     build: (pw.Context context) {
       return pw.Center(
@@ -35,7 +39,18 @@ Future makePdf() async {
     }
   );
 
+  final page2 = pw.Page(
+    pageTheme: pw.PageTheme( // ページのテーマ
+      pageFormat: PdfPageFormat.a4, // ついでにA4にして見る
+      theme: pw.ThemeData.withFont(base: font), // フォントを設定
+    ),
+    build: (pw.Context context) {
+      return pw.Center(
+        child: pw.Text("テキスト"),
+      ); // Center
+    }
+  ); 
   pdf.addPage(page); 
-
+  pdf.addPage(page2); 
   return pdf;
 }
